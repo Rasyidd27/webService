f13e5f985f71e6820c22a4e56f1747144e1e65ec
Rasyidd27 <rasyid20ridho@gmail.com>
2025-04-17 11:51:56 +0700

    Edit View

---
+++ resources/views/detail_Service/tampilDetailService.blade.php
@@ -0,0 +1,83 @@
+@include('layout.header')
+<!--begin::App Wrapper-->
+<div class="app-wrapper">
+    <!--begin::Header-->
+    @include('layout.navbar')
+    <!--end::Header-->
+    <!--begin::Sidebar-->
+    @include('layout.sidebar')    
+    <!--end::Sidebar-->
+    <!--begin::App Main-->
+    <main class="app-main">
+        <div class="app-content-header">
+            <!--begin::Container-->
+            <div class="container-fluid">
+                <!--begin::Row-->
+                <div class="row">
+                    <div class="col-sm-6"><h3 class="mb-0">Detail Service</h3></div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-end">
+                            <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Detail Service</li>
+                        </ol>
+                    </div>
+                </div>
+                <!--end::Row-->
+            </div>
+            <!--end::Container-->
+        </div>
+        <div class="app-content">
+            <!--begin::Container-->
+            <div class="container-fluid">
+                <!--begin::Row-->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <div class="row">
+                                    <div class="col-11">
+                                        <h3 class="card-title">Data Detail Service</h3>
+                                    </div>
+                                    <div class="col-1 d-flex justify-content-end">
+                                        <a href="{{ route('detail-service.create') }}" class="btn btn-success btn-sm">Tambah</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-body p-0">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Sparepart</th>
+                                            <th>Harga</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach($data as $row)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>{{ $row->sparepart }}</td>
+                                                <td>{{ $row->harga }}</td>
+                                                <td>
+                                                    <a href="{{ route('detail-service.edit', $row->id_detail_service) }}" class="btn btn-warning">Edit</a>  
+                                                    <span class="separator"></span>
+                                                    <a href="{{ route('detail-service.delete', $row->id_detail_service) }}" class="btn btn-danger">Delete</a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <!--end::App Main-->
+    <!--begin::Footer-->
+    @include('layout.kaki')
+    <!--end::Footer-->
+</div>
+@include('layout.scripts')
---
+++ resources/views/jenis_Kendaraan/tambahJenisKendaraan.blade.php
@@ -0,0 +1,103 @@
+@include('layout.header')
+<!--begin::App Wrapper-->
+<div class="app-wrapper">
+    <!--begin::Header-->
+    @include('layout.navbar')
+    <!--end::Header-->
+    <!--begin::Sidebar-->
+    @include('layout.sidebar')
+    <!--end::Sidebar-->
+    <!--begin::App Main-->
+    <main class="app-main">
+        <div class="app-content-header">
+            <!--begin::Container-->
+            <div class="container-fluid">
+                <!--begin::Row-->
+                <div class="row">
+                    <div class="col-sm-6"><h3 class="mb-0">Jenis Kendaraan</h3></div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-end">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Jenis Kendaraan</li>
+                            <li class="breadcrumb-item active" aria-current="page">Tambah</li>
+                        </ol>
+                    </div>
+                </div>
+                <!--end::Row-->
+            </div>
+            <!--end::Container-->
+        </div>
+        <div class="app-content">
+            <!--begin::Container-->
+            <div class="container-fluid">
+                <!--begin::Row-->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card card-primary card-outline mb-4">
+                            <!--begin::Header-->
+                            <div class="card-header">
+                                <div class="card-title">Data Jenis Kendaraan</div>
+                            </div>
+                            <!--end::Header-->
+                            <!--begin::Form-->
+                            <form class="needs-validation" action="{{ route('jenis-kendaraan.store') }}" method="post" novalidate>
+                                <!--begin::Body-->
+                                @csrf
+                                <div class="card-body">
+                                    <div class="row g-3">
+                                        <div class="col-md-6">
+                                            <label class="form-label">Jenis Kendaraan</label>
+                                            <input type="text" class="form-control" name="nama_jenis_kendaraan"
+                                                placeholder="Masukkan Jenis Kendaraan" required
+                                            />
+                                            <div class="invalid-feedback">Masukkan Jenis Kendaraan !!!</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!--end::Body-->
+                                <!--begin::Footer-->
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Simpan</button>
+                                </div>
+                                <!--end::Footer-->
+                            </form>
+                            <!--end::Form-->
+                            <!--start::Script Validation-->
+                            <script>
+                                // Example starter JavaScript for disabling form submissions if there are invalid fields
+                                (() => {
+                                    'use strict';
+
+                                    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                                    const forms = document.querySelectorAll('.needs-validation');
+
+                                    // Loop over them and prevent submission
+                                    Array.from(forms).forEach((form) => {
+                                        form.addEventListener(
+                                            'submit',
+                                            (event) => {
+                                                if (!form.checkValidity()) {
+                                                    event.preventDefault();
+                                                    event.stopPropagation();
+                                                }
+
+                                                form.classList.add('was-validated');
+                                            },
+                                            false,
+                                        );
+                                    });
+                                })();
+                            </script>
+                            <!--end::Script Validation-->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <!--end::App Main-->
+    <!--begin::Footer-->
+    @include('layout.kaki')
+    <!--end::Footer-->
+</div>
+@include('layout.scripts')
